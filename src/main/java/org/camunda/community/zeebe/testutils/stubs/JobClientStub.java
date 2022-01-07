56e99116d9acbe9438b93800ab8e1390a9aa1e4a
pihme <pihme@users.noreply.github.com>
2022-01-07 14:11:26 +0100

    chore: add missing methods

--- src/main/java/org/camunda/community/zeebe/testutils/stubs/JobClientStub.java
+++ src/main/java/org/camunda/community/zeebe/testutils/stubs/JobClientStub.java
@@ -3,6 +3,7 @@ package org.camunda.community.zeebe.testutils.stubs;
 import io.camunda.zeebe.client.api.command.CompleteJobCommandStep1;
 import io.camunda.zeebe.client.api.command.FailJobCommandStep1;
 import io.camunda.zeebe.client.api.command.ThrowErrorCommandStep1;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,13 +21,28 @@ public class JobClientStub implements JobClient {
   }
 
   @Override
+  public CompleteJobCommandStep1 newCompleteCommand(final ActivatedJob job) {
+    return newCompleteCommand(job.getKey());
+  }
+
+  @Override
   public FailJobCommandStep1 newFailCommand(final long jobKey) {
     return new FailJobCommandStep1Stub(takeJob(jobKey));
   }
 
   @Override
+  public FailJobCommandStep1 newFailCommand(final ActivatedJob job) {
+    return newFailCommand(job.getKey());
+  }
+
+  @Override
   public ThrowErrorCommandStep1 newThrowErrorCommand(final long jobKey) {
     return new ThrowErrorCommandStep1Stub(takeJob(jobKey));
+  }
+
+  @Override
+  public ThrowErrorCommandStep1 newThrowErrorCommand(final ActivatedJob job) {
+    return newThrowErrorCommand(job.getKey());
   }
 
   private ActivatedJobStub takeJob(final long jobKey) {
